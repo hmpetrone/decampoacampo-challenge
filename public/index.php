6c0a742257d40c4b181e89386b30712185cbbc82
hmpetrone <hernan_hmp@hotmail.com>
2025-01-20 09:47:09 -0300

    headers allow origin

--- public/index.php
+++ public/index.php
@@ -2,6 +2,17 @@
 // punto de entrada del servidor
 const BASE_PATH = __DIR__;
 
+//para que funcionen las peticiones desde cualquier origen.
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once BASE_PATH . '/autoload.php';
 require_once BASE_PATH . '/app/route_manager.php';
